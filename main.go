7983a3a03da45262f12c411e70e42926907244be
lc-1010 <532398960@qq.com>
2023-06-25 14:43:26 +0800

    <test>: Update to Test

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME))
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-
-    // 执行 SQL 操作
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
